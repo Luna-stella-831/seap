ee5dc275d431b58ad5371fda19c58860cfd3b4ea
k-hasegw <u087279c@ecs.osaka-u.ac.jp>
2021-11-23 13:26:32 +0900

    repair url

--- api/routes/commitRoutes.ts
+++ api/routes/commitRoutes.ts
@@ -1,26 +1,25 @@
 import * as control from "../controllers/commitController";
-const path = require('path');
+const path = require("path");
 
 export function routes(app) {
-  app.route("/seap/api/progress/:uid").get(control.load_commit);
+	app.route("/seap/api/progress/:uid").get(control.load_commit);
 
-  app.route("/seap/api/status/:uid").get(control.status);
+	app.route("/seap/api/status/:uid").get(control.status);
 
-  // renew (for test)
-  app.route("/seap/api/renew/:uid").get(control.renew);
+	// renew (for test)
+	app.route("/seap/api/renew/:uid").get(control.renew);
 
-  // renew (for gitbucket webhook)
-  app.route("/seap/api/renew/").post(control.renew);
+	// renew (for gitbucket webhook)
+	app.route("/seap/api/renew/").post(control.renew);
 
-  // all (create main screen)
-  app.route("/seap/api/all").get(control.all);
+	// all (create main screen)
+	app.route("/seap/api/all").get(control.all);
 
+	app.get("/seap", (req, res) => {
+		res.sendFile(path.join(__dirname, "/index.html"));
+	});
 
-  app.get('/seap/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  });
-
-  app.get('/seap/main.js', (req, res) => {
-    res.sendFile(path.join(__dirname, '/main.js'));
-  });
+	app.get("/seap/main.js", (req, res) => {
+		res.sendFile(path.join(__dirname, "/main.js"));
+	});
 }
