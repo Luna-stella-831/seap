9609478ca3d3471289e51eab0b87d77bd5f183f7
Luna-stella-831 <82633190+Luna-stella-831@users.noreply.github.com>
2022-01-13 20:51:27 +0900

    seap ver1.2.1

--- api/routes/main.js
+++ api/routes/main.js
@@ -36,8 +36,8 @@ window.onload = () => {
 //const endpoint = 'http://172.16.1.114:3000/seap/'; //@lab
 //const endpoint = 'http://192.168.2.102:3000/seap/'; //@home
 //const endpoint = 'http://10.11.191.249:3000/seap/'; //@odins-1x
-const endpoint = 'http://localhost:3000/seap/'; //@local
-//const endpoint = "https://loki.ics.es.osaka-u.ac.jp/seap/";
+//const endpoint = 'http://localhost:3000/seap/'; //@local
+const endpoint = "https://loki.ics.es.osaka-u.ac.jp/seap/";
 let endpointAllApi = endpoint + "api/all";
 
 const uid = new URL(document.location.href).searchParams.get("uid");
